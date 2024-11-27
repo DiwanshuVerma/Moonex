e92b763c54dbc95046d6b0156c88bd00c603d086
diwanshu verma <diwanshu63019@gmail.com>
2024-11-27 23:40:54 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { MainPage } from './sections/MainPage'
 import { About } from './sections/About'
@@ -8,7 +6,6 @@ import Faqs from './sections/Faqs'
 import Footer from './sections/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='!scroll-smooth bg-[#061523] min-h-screen overflow-x-hidden'>
